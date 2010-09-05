2917c05546a84f65b9e924b63dd5300e00479db4
ignaciopalomo <ignacio.palomo.duarte@gmail.com>
2010-09-05 18:47:35 +0000

    backup commit 2

--- ai.py
+++ ai.py
@@ -46,47 +46,52 @@ class AI:
         self.players = {}
 
     def create_players(self, p1, p2, p3, p4):
-        if p1 != "h":
-            mod_name  = "JUGADOR1"
-            mod_body  = '(import MAIN deftemplate ?ALL)'
-            self.players[1] = clips.BuildModule(mod_name, mod_body)
-            clips.Load(tool.ai('easy')) # FIXME mod to enable select a different difficult level
-        if p2 != "h":
-            mod_name  = "JUGADOR2"
-            mod_body  = '(import MAIN deftemplate ?ALL)'
-            self.players[2] = clips.BuildModule(mod_name, mod_body)
-            clips.Load(tool.ai('easy')) # FIXME
-        if p3 != "h":
-            mod_name  = "JUGADOR3"
-            mod_body  = '(import MAIN deftemplate ?ALL)'
-            self.players[3] = clips.BuildModule(mod_name, mod_body)
-            clips.Load(tool.ai('easy')) # FIXME
-        if p4 != "h":
-            mod_name  = "JUGADOR4"
-            mod_body  = '(import MAIN deftemplate ?ALL)'
-            self.players[4] = clips.BuildModule(mod_name, mod_body)
-            clips.Load(tool.ai('easy')) # FIXME
+        mod_name  = "JUGADOR1"
+        mod_body  = '(import MAIN deftemplate ?ALL)'
+        self.players[1] = clips.BuildModule(mod_name, mod_body)
+        clips.Load(tool.ai('easy')) # FIXME mod to enable select a different difficult level
+
+        mod_name  = "JUGADOR2"
+        mod_body  = '(import MAIN deftemplate ?ALL)'
+        self.players[2] = clips.BuildModule(mod_name, mod_body)
+        clips.Load(tool.ai('easy')) # FIXME
+
+        mod_name  = "JUGADOR3"
+        mod_body  = '(import MAIN deftemplate ?ALL)'
+        self.players[3] = clips.BuildModule(mod_name, mod_body)
+        clips.Load(tool.ai('easy')) # FIXME
+
+        mod_name  = "JUGADOR4"
+        mod_body  = '(import MAIN deftemplate ?ALL)'
+        self.players[4] = clips.BuildModule(mod_name, mod_body)
+        clips.Load(tool.ai('easy')) # FIXME
 
     def put_tile(self, player, board, players_tiles):
         self.players[player].SetFocus()
         clips.Run(1)
         output = clips.StdoutStream.Read()
-        return parse_tile_info(output)
+        return self.parse_tile_info(output)
 
-    def add_move(self, player, tile, side):
-        for k, v in self.players:
+    def add_move(self, step_counter, player, tile, side):
+        for k in self.players:
             self.players[k].SetFocus()
-            clips.BuildDeffacts("movimiento", "(jugador "+player+")(ficha "+tile+")(lado "+side+")")
+            d_name  = "movimiento"
+            d_body  = "(turno "+ str(step_counter) +")"
+            d_body += "(jugador "+ str(player) +")"
+            d_body += "(tiempo 3)"
+            d_body += "(ficha "+ tile +")"
+            d_body += "(lado "+ side +")"
+            clips.BuildDeffacts(d_name, d_body)
 
     def update_header(self, counter, left_side, right_side):
         # FIXME
         # quitar la regla header y poner la nueva - http://pyclips.sourceforge.net/manual/pyclips-objects.html - Retract()
-        for k, v in self.players:
+        for k in self.players:
             self.players[k].SetFocus()
             rule_name  = 'temporal'
             rule_prec  = '(declare (salience 99))'
             rule_prec += 'h1<-(header ?h)'
-            rule_body  = 'modify ?h1 (ficha_izq '+ left_side+') (ficha_der '+ right_side +') (turno '+ counter +'))'
+            rule_body  = 'modify ?h1 (ficha_izq '+ str(left_side) +') (ficha_der '+ str(right_side) +') (turno '+ str(counter) +'))'
             delete_me = clips.BuildRule(rule_name, rule_prec, rule_body)
             clips.Run(1)
             delete_me.Remove()
@@ -94,7 +99,7 @@ class AI:
 
     def set_player_tile(self, player, tile):
         self.players[player+1].SetFocus()
-        clips.BuildDeffacts("ficha", "(jugador "+player+")(ficha_izq "+tile[0]+")(ficha_der "+tile[1]+")")
+        clips.BuildDeffacts("ficha", "(jugador "+ str(player) +")(ficha_izq "+ str(tile[0]) +")(ficha_der "+ str(tile[1]) +")")
 
     def parse_tile_info(self, output):
         """ Must return new_tile, side""" 
