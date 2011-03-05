d89fe47c0fac505447705b2f5e5e9c138dfb5f1c
ignaciopalomo <ignacio.palomo.duarte@gmail.com>
2011-03-05 18:47:00 +0000

    Lab mode

--- lab.py
+++ lab.py
@@ -20,6 +20,18 @@ class Lab:
         self.current_match = 0
         self.team1_matches = 0
         self.team2_matches = 0
+        
+        self.po1_from = 375
+        self.po1_cur = 375
+        self.po1_to = 375
+        self.po1_eggs = 0
+        self.po2_from = 375
+        self.po2_cur = 37
+        self.po2_to = 375
+        self.po2_eggs = 0
+        
+        self.font_main = SpriteFont("fonts/Comfortaa Regular.ttf", 24, False, False, 32, 255)
+        
     def draw(self):
         #str(self.domino.points_team1())
         #self.team1_matches
@@ -30,17 +42,36 @@ class Lab:
         self.p2.draw((465, 25))
         self.p4.draw((625, 25))
 
-        po1 = 375+((self.team1_matches-self.team2_matches)*3.75)
-        po2 = 375+((self.team2_matches-self.team1_matches)*3.75)
-        Gloss.draw_box((25, 200), po1, 50, 0.0, (0, 0), 1, Color.from_html("#f9b00c"))
-        Gloss.draw_box((25, 200), po1, 10, 0.0, (0, 0), 1, Color.from_html("#febf1a"))
-        Gloss.draw_box((775-po2, 200), po2, 50, 0.0, (0, 0), 1, Color.from_html("#cccccc"))
-        Gloss.draw_box((775-po2, 200), po2, 10, 0.0, (0, 0), 1, Color.from_html("#e0e0e0"))
+        po1_new = 375+((self.team1_matches-self.team2_matches)*3.75)
+        po2_new = 750-po1_new
+        
+        if po1_new != self.po1_to:
+            self.po1_to = po1_new
+            self.po1_from = self.po1_cur
+            self.po1_eggs = 0
+            self.po2_to = po2_new
+            self.po2_from = self.po2_cur
+            self.po2_eggs = 0
+        
+        self.po1_eggs += Gloss.elapsed_seconds * 2
+        self.po2_eggs += Gloss.elapsed_seconds * 2
+        self.po1_cur = Gloss.smooth_step(self.po1_from, self.po1_to, self.po1_eggs)
+        self.po2_cur = Gloss.smooth_step(self.po2_from, self.po2_to, self.po2_eggs)
+        
+        Gloss.draw_box((25, 200), self.po1_cur, 50, 0.0, (0, 0), 1, Color.from_html("#f9b00c"))
+        Gloss.draw_box((25, 200), self.po1_cur, 10, 0.0, (0, 0), 1, Color.from_html("#febf1a"))
+        Gloss.draw_box((775-self.po2_cur, 200), self.po2_cur, 50, 0.0, (0, 0), 1, Color.from_html("#cccccc"))
+        Gloss.draw_box((775-self.po2_cur, 200), self.po2_cur, 10, 0.0, (0, 0), 1, Color.from_html("#e0e0e0"))
 
         # los puntos de cada partida van de 0 a 200
         Gloss.draw_box((25, 275), (self.domino.points_team1()*375)/200, 25, 0.0, (0, 0), 1, Color.from_html("#f9b00c"))
         Gloss.draw_box((775, 275), (self.domino.points_team2()*375)/200, 25, 0.0, ((self.domino.points_team2()*375)/200, 0), 1, Color.from_html("#cccccc"))
 
+        # pintamos las partidas ganadas encima de la barra
+        text2_size = self.font_main.measure_string(str(self.team2_matches))
+        self.font_main.draw(str(self.team1_matches), position = (35, 220), color = Color.WHITE)
+        self.font_main.draw(str(self.team2_matches), position = (765-text2_size[0], 220), color = Color.WHITE)
+        
         #Gloss.elapsed_seconds
     def update(self):
         if self.status == 0:
@@ -59,8 +90,8 @@ class Lab:
             else:
                 self.current_match += 1
                 if self.domino.points_team1() > self.domino.points_team2(): 
-                    self.team1_matches += 1                    
-                else:             
+                    self.team1_matches += 1 
+                else:
                     self.team2_matches += 1
                 self.domino.restart()
                 self.status = 1
