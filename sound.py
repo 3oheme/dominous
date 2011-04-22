461190a4eaad0306968a5a4985cbddb02b6fb63e
ignaciopalomo <ignacio.palomo.duarte@gmail.com>
2011-04-22 16:53:42 +0000

    disable sound in linux, so it works at least

--- sound.py
+++ sound.py
@@ -8,28 +8,32 @@ import random
 
 class Sound:
     def __init__(self):
-        pass
-        pygame.mixer.init(44100, 16, 2, 4096)
-        self.tile_sound = [
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile1.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile2.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile3.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile4.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile5.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile6.ogg")),
-            pygame.mixer.Sound(tool.sound("theme_tile", "tile7.ogg"))]
-        self.intro_sound = pygame.mixer.Sound(tool.sound("system", "intro_dominous.ogg"))
-        self.menu_sound = pygame.mixer.Sound(tool.sound("system", "menu.ogg"))
-        self.credits_sound = pygame.mixer.Sound(tool.sound("system", "credits.ogg"))
+        if os.environ['OS'] == 'Windows_NT':
+            pygame.mixer.init(44100, 16, 2, 4096)
+            self.tile_sound = [
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile1.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile2.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile3.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile4.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile5.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile6.ogg")),
+                pygame.mixer.Sound(tool.sound("theme_tile", "tile7.ogg"))]
+            self.intro_sound = pygame.mixer.Sound(tool.sound("system", "intro_dominous.ogg"))
+            self.menu_sound = pygame.mixer.Sound(tool.sound("system", "menu.ogg"))
+            self.credits_sound = pygame.mixer.Sound(tool.sound("system", "credits.ogg"))
     def tile(self):
-        self.tile_sound[random.randrange(0,7)].play()
+        if os.environ['OS'] == 'Windows_NT':
+            self.tile_sound[random.randrange(0,7)].play()
     def intro(self):
-        self.intro_sound.play()
+        if os.environ['OS'] == 'Windows_NT':
+            self.intro_sound.play()
     def menu(self):
-        pygame.mixer.fadeout(500)
-        self.menu_sound.play()
+        if os.environ['OS'] == 'Windows_NT':
+            pygame.mixer.fadeout(500)
+            self.menu_sound.play()
     def credits(self):
-        pygame.mixer.fadeout(1000)
-        self.credits_sound.play()
+        if os.environ['OS'] == 'Windows_NT':
+            pygame.mixer.fadeout(1000)
+            self.credits_sound.play()
 
 sound = Sound()
